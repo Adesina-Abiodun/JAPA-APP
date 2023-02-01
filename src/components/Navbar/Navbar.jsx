35af1370fb82e0115f83ddc0c772d202454425b4
Daniel Abati <danielabatibabatunde1@gmail.com>
2023-02-01 02:13:24 +0100

    responsive-halfway

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../img/logo-nav.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
   return (
     <div className="nav-section">
       <nav>
         <div className="nav-title">
-          <img loading='lazy' src={logo} alt={logo} />
+          <img loading="lazy" src={logo} alt={logo} />
         </div>
         <div className="nav-link">
-          <ul>
+          <ul className={click ? "nav-menu actives" : "nav-menu"}>
             <li>
               {/* replace with Link */}
               <span>
@@ -23,15 +26,28 @@ const Navbar = () => {
               </span>
             </li>
             <li>
-              <span><p>How it works</p></span>
+              <span>
+                <p>How it works</p>
+              </span>
             </li>
             <li>
-              <span><p>Features</p></span>
+              <span>
+                <p>Features</p>
+              </span>
             </li>
             <li>
-              <span><p>Contacts</p></span>
+              <span>
+                <p>Contacts</p>
+              </span>
             </li>
           </ul>
+          <div className="hamburger" onClick={handleClick}>
+            {click ? (
+              <FaTimes size={30} style={{ color: "#59CC7F" }} />
+            ) : (
+              <FaBars size={30} style={{ color: "#59CC7F" }} />
+            )}
+          </div>
         </div>
       </nav>
     </div>
